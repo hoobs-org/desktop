812786bc3e374a1adfd887b4c174664e6fedd9ed
mkellsy <mkellsy@gmail.com>
2021-03-26 03:05:03 -0600

    move config to a get

--- src/components/dialogs/popup.vue
+++ src/components/dialogs/popup.vue
@@ -18,8 +18,11 @@
                 this.$refs.frame.addEventListener("load", () => {
                     this.$refs.frame.contentWindow.$hoobs = this.$hoobs;
                     this.$refs.frame.contentWindow.$bridge = this.bridge;
-                    this.$refs.frame.contentWindow.$config = this.options.items;
                     this.$refs.frame.contentWindow.$close = () => { this.$dialog.close("plugin"); };
+
+                    Object.defineProperty(this.$refs.frame.contentWindow, "$config", {
+                        get: () => this.options.items,
+                    });
 
                     Object.defineProperty(this.$refs.frame.contentWindow, "$value", {
                         get: () => fetch(),

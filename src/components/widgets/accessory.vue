6070dbc706f31545d6c04f87e5f11f220125e77c
mkellsy <mkellsy@gmail.com>
2021-04-06 12:16:53 -0600

    accessory widget aspect ratio

--- src/components/widgets/accessory.vue
+++ src/components/widgets/accessory.vue
@@ -76,8 +76,11 @@
         user-select: none;
 
         .device {
-            width: 100%;
-            height: 100%;
+            max-width: 100%;
+            max-height: 100%;
+            margin: auto;
+            aspect-ratio: 155/214;
+            object-fit: contain;
             display: flex;
             flex-direction: row;
             justify-content: space-around;

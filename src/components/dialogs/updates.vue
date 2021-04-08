67d6e7348cc57a3376b93a71b46d04977976c0de
mkellsy <mkellsy@gmail.com>
2021-04-08 10:02:51 -0600

    add plugin name to plugin updates

--- src/components/dialogs/updates.vue
+++ src/components/dialogs/updates.vue
@@ -20,8 +20,8 @@
                         {{ $t("version_node") }}: {{ status.node_current }}
                         <span class="value">{{ $t("available") }}</span>
                     </div>
-                    <div v-if="!loading && plugins.length > 0" class="row">
-                        {{ $t("plugins") }}: {{ plugins.length }} {{ $t("plugin_updates") }}
+                    <div v-for="(plugin, index) in plugins" :key="`plugin:${index}`" class="row">
+                        {{ $hoobs.repository.title(plugin.name) }}: {{ plugin.latest }}
                         <span class="value">{{ $t("available") }}</span>
                     </div>
                     <div v-if="!loading && !updated" class="row" style="margin-top: 7px;">

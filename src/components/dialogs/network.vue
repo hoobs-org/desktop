e62e50406c4bde155009961c2088bffe06ee63f4
mkellsy <mkellsy@gmail.com>
2021-09-10 23:00:47 -0600

    network dialog

--- src/components/dialogs/network.vue
+++ src/components/dialogs/network.vue
@@ -20,7 +20,32 @@
     <modal :title="$t('network')" :draggable="true" width="780px" height="820px">
         <div id="network">
             <div v-if="!loading" class="content">
-                <div class="form"></div>
+                <div class="form">
+                    <div class="stack">
+                        <div v-if="connected" class="item">
+                            <div class="name">{{ $t("status") }}</div>
+                            <div class="value">{{ $t("connected") }}</div>
+                        </div>
+                        <div class="item">
+                            <div class="name">{{ $t("wifi") }}</div>
+                            <div class="value">
+                                <checkbox v-model="wireless" />
+                            </div>
+                        </div>
+                        <div v-if="connection" class="item">
+                            <div class="name">{{ connection.ssid }}</div>
+                            <signal style="margin-right: 5px;" :quality="connection.quality" :secure="connection.security.mode && connection.security.mode !== '' && connection.security.mode !== 'none'" />
+                        </div>
+                    </div>
+                    <div class="row section" style="margin: 0;">{{ $t("networks") }}</div>
+                    <div class="networks">
+                        <div v-for="(network, index) in networks" :key="`network:${index}`" class="item">
+                            <div class="name">{{ network.ssid }}</div>
+                            <signal :quality="network.quality" :secure="network.security.mode && network.security.mode !== '' && network.security.mode !== 'none'" />
+                            <chevron class="chevron" />
+                        </div>
+                    </div>
+                </div>
             </div>
             <div v-else class="status">
                 <div class="loading">
@@ -41,10 +66,20 @@
     export default {
         name: "netwsork",
 
+        components: {
+            "signal": () => import(/* webpackChunkName: "common" */ "@/components/elements/signal.vue"),
+            "chevron": () => import(/* webpackChunkName: "common" */ "@/components/elements/chevron.vue"),
+        },
+
         data() {
             return {
                 loading: true,
-                status: {},
+                scanning: true,
+                connected: false,
+                connection: null,
+                devices: [],
+                hotspot: {},
+                wireless: false,
                 networks: [],
                 messages: [],
                 message: "",
@@ -52,10 +87,19 @@
         },
 
         async mounted() {
-            this.status = await this.$hoobs.network.status();
-            this.networks = await this.$hoobs.networks();
+            const status = (await this.$hoobs.network.status()) || {};
 
             this.loading = false;
+
+            this.connected = status.connected;
+            [this.connection] = status.connections;
+            this.devices = status.devices;
+            this.hotspot = status.hotspot;
+            this.wireless = status.wireless;
+
+            if (this.wireless) this.networks = (await this.$hoobs.networks()).filter((item) => item.ssid !== this.connection.ssid);
+
+            this.scanning = false;
         },
     };
 </script>
@@ -66,5 +110,84 @@
         display: flex;
         flex-direction: column;
         margin: 0 0 0 10px;
+        overflow: hidden;
+
+        .content {
+            overflow: hidden;
+
+            .form {
+                overflow: hidden;
+
+                .stack {
+                    display: flex;
+                    flex-direction: column;
+
+                    .item {
+                        display: flex;
+                        align-items: center;
+                        padding: 14px 7px;
+                        border-top: var(--modal-border) 1px solid;
+
+                        &:first-child {
+                            border-top: 0 none;
+                        }
+
+                        .name {
+                            flex: 1;
+                            opacity: 0.7
+                        }
+                    }
+                }
+
+                .networks {
+                    flex: 1;
+                    overflow: auto;
+
+                    .item {
+                        display: flex;
+                        align-items: center;
+                        padding: 14px 7px;
+                        border-top: var(--modal-border) 1px solid;
+                        cursor: pointer;
+
+                        &:first-child {
+                            border-top: 0 none;
+                        }
+
+                        .name {
+                            flex: 1;
+                            opacity: 0.7;
+                        }
+
+                        .chevron {
+                            opacity: 0.7;
+                        }
+
+                        &:hover {
+                            .name {
+                                opacity: 1;
+                            }
+
+                            .chevron {
+                                opacity: 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        .status {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+            margin: 0 14px 0 0;
+
+            .loading {
+                flex: 1;
+                padding: 0 0 4% 0;
+            }
+        }
     }
 </style>
---
+++ src/components/elements/chevron.vue
@@ -0,0 +1,49 @@
+<!-------------------------------------------------------------------------------------------------
+ | hoobs-desktop                                                                                  |
+ | Copyright (C) 2021 HOOBS                                                                       |
+ |                                                                                                |
+ | This program is free software: you can redistribute it and/or modify                           |
+ | it under the terms of the GNU General Public License as published by                           |
+ | the Free Software Foundation, either version 3 of the License, or                              |
+ | (at your option) any later version.                                                            |
+ |                                                                                                |
+ | This program is distributed in the hope that it will be useful,                                |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of                                 |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  |
+ | GNU General Public License for more details.                                                   |
+ |                                                                                                |
+ | You should have received a copy of the GNU General Public License                              |
+ | along with this program.  If not, see <http://www.gnu.org/licenses/>.                          |
+ -------------------------------------------------------------------------------------------------->
+
+<template>
+    <div class="chevron">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path class="base" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+        </svg>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "chevron",
+    };
+</script>
+
+<style lang="scss" scoped>
+    .chevron {
+        width: 24px;
+        height: 24px;
+        margin-left: 10px;
+        user-select: none;
+
+        svg {
+            width: 24px;
+            height: 24px;
+        }
+
+        .base {
+            fill: var(--modal-text);
+        }
+    }
+</style>

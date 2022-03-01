a90b69822ba9ff0532f851c0adf4eb349ec8a5b5
mkellsy <mkellsy@gmail.com>
2022-03-01 03:01:02 -0700

    change default loading state

--- src/layouts/authenticated.vue
+++ src/layouts/authenticated.vue
@@ -52,7 +52,7 @@
         data() {
             return {
                 reload: false,
-                loading: true,
+                loading: false,
                 dialogs: 0,
             };
         },

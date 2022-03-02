6869384df7beccac7bac8be93330b6c855a622a9
mkellsy <mkellsy@gmail.com>
2022-03-02 16:38:17 -0700

    reduce the number of chunks during build

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ const Monaco = require("monaco-editor-webpack-plugin");
 
 module.exports = {
     pluginOptions: { electronBuilder: { outputDir: "dist", nodeIntegration: true } },
-    configureWebpack: { plugins: [new Monaco({ languages: ["json"] })] },
+
+    configureWebpack: {
+        plugins: [new Monaco({ languages: ["json"] })],
+        performance: { hints: false },
+        optimization: { runtimeChunk: "single" },
+    },
 };
